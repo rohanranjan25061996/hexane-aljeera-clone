30e64790224a7f670ead3f09fb24324445d3efb7
Rohan Ranjan <49858287+rohanranjan25061996@users.noreply.github.com>
2021-03-10 20:42:43 +0530

    Update App.js

--- project-alajeera/src/App.js
+++ project-alajeera/src/App.js
@@ -3,16 +3,9 @@ import { AllRoutes } from './Routes/Routes';
 
 function App() {
   return (
-<<<<<<< HEAD
-    <Router>
-      <Navbar />
-      <FooterContainer/>
-    </Router>  
-=======
     <div className="App">
       <AllRoutes />
     </div>
->>>>>>> 3721e79357a4b103546528798c0b3ccfdc58aa2c
   );
 }
 
