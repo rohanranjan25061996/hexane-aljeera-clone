b6752dd286fa579ee437b89d5509c273e2ada28c
Siddharth308 <prabhu.sid97@gmail.com>
2021-03-11 11:14:20 +0530

    corona page

--- project-alajeera/src/Redux/store.js
+++ project-alajeera/src/Redux/store.js
@@ -1,29 +1,24 @@
-import { applyMiddleware, compose, createStore,combineReducers } from "redux";
-import { reducer } from "./reducer";
-// import ReduxThunk from "redux-thunk";
-// const composeEnhancer =
-//   (typeof Window !== "undefined" &&
-//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-//   compose;
-// const enhancer = composeEnhancer(applyMiddleware(ReduxThunk));
-// export const store = createStore(reducer, enhancer);
+import { applyMiddleware, compose, createStore, combineReducers } from "redux";
 
 import { economyReducer } from "./Economy/reducer";
+import { coronaReducer } from "./Corona/reducer";
 
+const rootReducer = combineReducers({
+  eco: economyReducer,
+  corona: coronaReducer,
+});
 
+const customMiddleWare = (store) => (next) => (action) => {
+  return typeof action === "function"
+    ? action(store.dispatch, store.getState)
+    : next(action);
+};
 
-const rootReducer = combineReducers( { 
-    eco: economyReducer
- } )
+const composeEnhancers =
+  (typeof window !== "undefined" &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
+const enchancer = composeEnhancers(applyMiddleware(customMiddleWare));
 
-const customMiddleWare = store => next => action =>{
-
-    return typeof action === "function" ? action(store.dispatch, store.getState) : next(action);
-}
-
-const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
-
-const enchancer = composeEnhancers(applyMiddleware(customMiddleWare))
-
-export const store = createStore( rootReducer, enchancer )
+export const store = createStore(rootReducer, enchancer);
